fd843f6b4401c98a8d6248f8037e2243579967bb
Sikder Muktadir <mUKs@Sikders-MacBook-Pro.local>
2018-01-14 23:23:12 -0500

    Commit 3

---
+++ src/PassTesting.java
@@ -0,0 +1,16 @@
+package antSystemTesting;
+
+import org.testng.annotations.Test;
+
+public class PassTesting {
+
+@Test
+
+public void TestOne () {
+	
+	System.out.println("Hope this is going to run In Sha Allah");
+	
+}
+
+
+}
